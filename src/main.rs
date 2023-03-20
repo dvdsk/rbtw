c5eb27c9a95fa04286dab67389a665c537f9edcb
dvdsk <noreply@davidsk.dev>
2023-03-20 18:27:47 +0100

    adds help menu

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
 #![feature(unix_chown)]
 
-
+use std::env;
 use std::os::unix::fs::chown;
-
 use std::process::Command;
 
 mod efi;
 mod gid;
 
 const ROOT: u32 = 0;
+const HELP: &'static str =
+    "Usage: call without any arguments to restart to windows. \n\n\
+        - Requires sudo on its first run. \n\
+        - The next boot after this will be to the default OS again.\n\
+        - Will stop if no windows bootloader is found.\n\n\
+        Options:\n    --help, -h    Print this help message\n";
 
 fn main() {
+    if let Some(arg) = env::args().nth(1) {
+        if arg == "--help" || arg == "-h" {
+            println!("{HELP}");
+            return;
+        }
+    }
+
     sudo::escalate_if_needed().expect("sudo failed");
 
     let was_set = gid::is_set();

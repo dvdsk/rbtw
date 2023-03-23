422bb1ddf0c6ccb0f49091626c0fa95e2d64e893
dvdsk <noreply@davidsk.dev>
2023-03-23 16:18:34 +0100

    adds binary via CI and non nightly warning

---
+++ build.rs
@@ -0,0 +1,14 @@
+use rustc_version::{version_meta, Channel};
+
+fn main() {
+    // Set cfg flags depending on release channel
+    if let Ok(Channel::Nightly) = version_meta().map(|m| m.channel) {
+        return;
+    } else {
+        panic!("\n*****************************\n \
+               Need nightly to compile. \
+               When installing from crates.io try using:\
+               \n\t`cargo install +nightly`\
+               \n******************************");
+    }
+}
